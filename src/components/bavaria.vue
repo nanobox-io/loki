90b84d197718982dcaeb25ed8f4b7a6a584d3b4d
Mark Parson <mark@nanobox.io>
2017-11-15 09:11:49 -0700

    initial commit

---
+++ src/components/bavaria.vue
@@ -0,0 +1,21 @@
+<script type="text/babel">
+export default {
+  name: 'bavaria'
+}
+</script>
+
+<!--
+  **** H T M L ****
+-->
+
+<template lang="pug">
+  .bavaria I am bavaria
+</template>
+
+<!--
+  ***** C S S *****
+-->
+
+<style lang="scss" scoped>
+  .bavaria {}
+</style>
